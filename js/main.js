04a86f0947fe36fade9a2d01a7a7ff35c0ff6ead
Mun May Tee <munmay.g@gmail.com>
2015-04-01 14:38:46 -0700

    initial

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Show numbers from 0-100
+// 	Within those numbers, find number divisible by 3
+// 		Print fizz if number is divisible by 3
+
+// 	Within those numbers, find numbers divisible by 5
+// 		Print buzz if number is divisible by 5
+
+$(document).ready(function() {
+
+
+
+for(var i=0; i<101; i++){
+
+	var y = 0 + (i*20);
+
+    if (i%5 == 0 && i%3 == 0){
+	$('#numbers').append("fizzbuzz <br>");
+	}
+
+	else if (i%3 == 0){
+	$('#numbers').append("fizz <br>");
+	}
+
+	else if (i%5 == 0) {
+	$('#numbers').append("buzz <br>");
+	}
+
+	else{
+	$('#numbers').append(i + "<br>");
+	}
+	
+}
+
+});
